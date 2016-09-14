17260eb7f7e88812999ac27774c45536d741a60c
mujaffar <mujaffar.sanadi01@gmail.com>
2016-09-14 15:37:29 +0530

    asdfsadf

--- js/config.js
+++ js/config.js
@@ -1,1 +1,6 @@
-var serverHost = 'http://shivtraderssangli.com/getdata.aspx';
+//var serverHost = 'http://shivtraderssangli.com/getdata.aspx';
+
+var serverHost = 'http://localhost:56927/wwwroot/getdata.aspx';
+//http://vas.mobilogi.com/api.php?username=stipls&password=pass12345&route=1&sender=STIPLS&mobile[]=9503426967&message[]=TEST SMS';
+
+//serverHost = 'https://hello-world-mujaffar.c9users.io/hello-world.php';
